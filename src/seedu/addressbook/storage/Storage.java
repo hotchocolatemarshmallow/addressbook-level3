41af1294c0ece6d3d333fd2b9f35351ac3239481
hotchocolatemarshmallow <hotchocolatemarshmallow35211@gmail.com>
2016-09-22 08:54:00 +0800

    added interface class Storage that stores only , made StorageFile extend Storage. Declared storage variable to type Storage but used StorageFile constructor. But faced with this problem : "Unhandled exception type StorageFile.InvalidStorageFilePathException"

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public interface Storage {
+    
+    
+    public AddressBook load() throws StorageOperationException;
+    public String getPath();
+    public void save(AddressBook addressBook) throws StorageOperationException;
+    
+    
+
+}
